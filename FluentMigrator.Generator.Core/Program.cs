9b06dd24540405693d397e7d305a5d82c2b87f03
mp@mp <mp@mp.pl>
2022-02-07 15:48:39 +0100

    1.0.0.1 Generator

--- FluentMigrator.Generator.Core/Program.cs
+++ FluentMigrator.Generator.Core/Program.cs
@@ -1,40 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
-var name = args.AsQueryable().FirstOrDefault();
 
-var @namsepace = "Migrations";
+var directory = Directory.GetCurrentDirectory();
 
-if (string.IsNullOrEmpty(name))
+var directoryOfMigrations = Path.Combine(directory, "Migrations");
+
+Console.WriteLine(directory);
+
+var nameOfMigration = args?.AsQueryable()?.FirstOrDefault() ?? String.Empty;
+
+var namespaceOfMigration = "Migrations";
+
+if (string.IsNullOrEmpty(nameOfMigration))
 {
     Console.Write("Name:");
-    name = Console.ReadLine();
+    nameOfMigration = Console.ReadLine();
+    nameOfMigration = RemoveInvalidFilePathCharacters(nameOfMigration,"");
+
 }
 
 
 
-
-if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(@namsepace))
+if (!string.IsNullOrEmpty(nameOfMigration) && !string.IsNullOrEmpty(namespaceOfMigration))
 {
     var syntaxFactory = SF.CompilationUnit();
 
-    var @namespace = SF.NamespaceDeclaration(SF.ParseName(@namsepace))
+    var @namespace = SF.NamespaceDeclaration(SF.ParseName(namespaceOfMigration))
         .NormalizeWhitespace();
     
     syntaxFactory = syntaxFactory.AddUsings(
         SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("FluentMigrator")));
 
-    var date = DateTime.Now.Ticks;
+    var date = DateTime.Now.ToString("ddMMyyyyHHmmss");
     
     var attributeArgument = SyntaxFactory.AttributeArgument(
         null, null, SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression,
             SyntaxFactory.Literal(date)));
     
-    var classDeclaration = SF.ClassDeclaration(name)
+    var classDeclaration = SF.ClassDeclaration(nameOfMigration)
         .AddModifiers(SF.Token(SyntaxKind.PublicKeyword))
         .AddBaseListTypes(
         SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("Migration")))
@@ -67,10 +75,40 @@ if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(@namsepace))
         .NormalizeWhitespace()
         .ToFullString();
     
+   
+
+    if (!Directory.Exists(directoryOfMigrations))
+    {
+        Directory.CreateDirectory(directoryOfMigrations);
+    }
+    var filePathToMigration = Path.Combine(directoryOfMigrations, $"{date}_{nameOfMigration}.cs");
+    if (File.Exists(filePathToMigration))
+    {
+        Console.Write($"File Exists in path {filePathToMigration} overite  y/n (def n)? :");
+
+        var overite = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+        if (!overite)
+        {
+            Console.WriteLine("Aborting!");
+            return;
+        }
+    }
+    File.WriteAllText(filePathToMigration,code);
     Console.WriteLine(code);
+    
     
 }
 else
 {
     Console.WriteLine("Requirements not met");
+}
+
+
+string RemoveInvalidFilePathCharacters(string filename, string replaceChar)
+{
+    if (filename == null) throw new ArgumentNullException(nameof(filename));
+    if (replaceChar == null) throw new ArgumentNullException(nameof(replaceChar));
+    string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+    Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+    return r.Replace(filename, replaceChar).Replace(" ",replaceChar);
 }
